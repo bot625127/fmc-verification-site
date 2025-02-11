83974572f4ad2275ba1eb784e9ce4db76119c40d
bot625127 <abbybigboy@gmail.com>
2025-02-11 18:08:05 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("learn-more").addEventListener("click", function() {
+    alert("Verification ensures that only real users join FMC, preventing bots from causing harm. Protecting the community is our top priority.");
+});
